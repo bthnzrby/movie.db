f2a115cd0471bc3ad0ba881e4b637b3710517aa8
bthnzrby <xbthnzrbyx@gmail.com>
2023-08-01 12:27:34 +0300

    se

--- src/config/Url.tsx
+++ src/config/Url.tsx
@@ -1,10 +1,9 @@
 export const API_KEY = "api_key=8cc494f765248cf7c16c25c005a889a8"
 export const BASE_URL = "https://api.themoviedb.org/3"
-export const SEARCH_URL = BASE_URL + "/search/movie?" +API_KEY
-
-export const GENRE_URL = BASE_URL + "/genre/movie/list?" +API_KEY
+export const SEARCH_URL = BASE_URL + "/search/movie?" + API_KEY
+export const GENRE_URL = BASE_URL + "/genre/movie/list?" + API_KEY
 export const DETAIL_URL = BASE_URL + "/movie/"
-export const DISCOVER_URL = BASE_URL + "/discover/movie?" +API_KEY
+export const DISCOVER_URL = BASE_URL + "/discover/movie?" + API_KEY
 export const IMG_URL = "https://image.tmdb.org/t/p"
 export const IMG_SIZE_500 = "/w500"
 export const IMG_SIZE_1920 = "/w1920_and_h800_multi_faces/"
